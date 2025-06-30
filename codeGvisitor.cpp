312919d60033941cd1d6ff9ef0df31d2d72b0156
aseelFraira <asi.shib69@gmail.com>
2025-06-30 18:42:52 +0300

    fixing call function

--- codeGvisitor.cpp
+++ codeGvisitor.cpp
@@ -199,16 +199,18 @@ void codeGvisitor::visit(Call& node) {
         node.args->exps[i]->accept(*this);
         auto arg = node.args->exps[i];
         arg->accept(*this);
+        std::cout << "{DEBUG} 1" << std::endl;
+
         if (arg->type == ast::BuiltInType::STRING) {//
             cb->emitString(arg->newVar);
             std::string ptrVar = cb->freshVar();//
-            std::cout << "{DEBUG} 1" << std::endl;
+            std::cout << "{DEBUG} 2" << std::endl;
+
 
             int len = arg->newVar.length() + 1;
             cb->emit(ptrVar + " = getelementptr [" + std::to_string(len) + " x i8], [" +
                     std::to_string(len) + " x i8]* " + arg->newVar + ", i32 0, i32 0");
             argValues.push_back(ptrVar);
-            std::cout << "{DEBUG} 2" << std::endl;
 
         }
         else if (arg->type == ast::BuiltInType::BYTE &&
