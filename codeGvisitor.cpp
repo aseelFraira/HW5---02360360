32e5553b880cc254cb1111faf530351081fb74c9
aseelFraira <asi.shib69@gmail.com>
2025-07-01 15:51:43 +0300

    fsd

--- codeGvisitor.cpp
+++ codeGvisitor.cpp
@@ -223,6 +223,7 @@ void codeGvisitor::visit(Call& node) {
     for (int i = 0; i < node.args->exps.size(); i++) {
         const auto &arg = node.args->exps[i];
         arg->accept(*this);
+        std::cerr << "The value is " <<arg->newVar <<std::endl;
         argTypes.push_back(output::changeType(arg->type));
         if (arg->type == ast::BuiltInType::STRING) {//
             string strArray = cb->emitString(arg->newVar);
